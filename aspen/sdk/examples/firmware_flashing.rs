8932e8b30fdaba6a08353984ed092a442cb479ef
Aubrey Taylor <aubrey@hall.ly>
2026-01-19 22:10:31 -0600

    change logging

--- aspen/sdk/examples/firmware_flashing.rs
+++ aspen/sdk/examples/firmware_flashing.rs
@@ -4,7 +4,9 @@ use aspen_sdk::icu::{ICU, UpgradeTarget, usb::USBTransport};
 use tracing::{error, info};
 
 fn main() {
-  tracing_subscriber::fmt().init();
+  tracing_subscriber::fmt()
+    .with_max_level(tracing::Level::TRACE)
+    .init();
   let image = env::args().nth(1).expect("put the image path in the first argument");
   let image = fs::read(image).expect("failed to read image");
   let transport = USBTransport::first_device()
